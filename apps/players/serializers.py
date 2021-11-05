f53bafc0d35e91298f08b84fd24dab5b6ceb397a
Louack <loic.briset@hotmail.fr>
2021-11-05 23:15:31 +0100

    added tournaments_list field to PlayerDetailSerializer

--- apps/players/serializers.py
+++ apps/players/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from apps.players.models import Player
+from apps.tournaments.models import Participant
 
 
 class PlayerListSerializer(serializers.ModelSerializer):
@@ -16,19 +17,40 @@ class PlayerListSerializer(serializers.ModelSerializer):
 
 
 class PlayerDetailSerializer(serializers.ModelSerializer):
+    tournaments_list = serializers.SerializerMethodField()
+
     class Meta:
         model = Player
-        exclude = ('id', 'creator')
+        fields = (
+            'number',
+            'username',
+            'last_name',
+            'first_name',
+            'rank',
+            'avg_place',
+            'tournaments_played',
+            'tournaments_won',
+            'matches_played',
+            'matches_won',
+            'tournaments_list',
+            'date_created',
+        )
+
+    @staticmethod
+    def get_tournaments_list(instance):
+        tournaments = [
+            participant.tournament.number for participant in
+            Participant.objects.filter(player=instance)
+        ]
+        return tournaments
 
     def create(self, validated_data):
         creator = self.context['profile']
-
         player = Player.objects.create(
             creator=creator,
             username=validated_data['username'],
             last_name=validated_data['last_name'],
             first_name=validated_data['first_name']
         )
-
         player.save()
         return player
