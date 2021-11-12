ca0e57a4f0260eb057af1e7f3f31c7f39c3e0810
Louack <loic.briset@hotmail.fr>
2021-11-12 23:39:39 +0100

    reworked login form with react form hook and yup

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,45 +1,36 @@
-import React, { useState, useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import  { Link } from "react-router-dom";
-import axios from "axios";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import AuthContext from '../context/AuthContext';
+import * as yup from "yup";
+import axios from "axios";
+
+const schema = yup.object().shape({
+    username: yup.string().required("Nom d'utilisateur requis."),
+    password: yup.string().required("Mot de passe requis.")
+});
 
 const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [blankUsername, setBlankUsername] = useState(false)
-    const [blankPassword, setBlankPassword] = useState(false)
     const [errorMessage, setErrorMessage] = useState('')
-    let {getAuthTokens} = useContext(AuthContext)
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+    });
+    const { getAuthTokens } = useContext(AuthContext)
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        setBlankUsername(false);
-        setBlankPassword(false);
-        setErrorMessage('')
-        if (username === '' || password === '') {
-            if (username === '') {
-                setBlankUsername(true)
-            }
-            if (password === '') {
-                setBlankPassword(true)
-            } 
-        } else {
-            let loginData = {
-                username,
-                password,
-            };
-            try {
-                let res = await axios.post('/api/token/', loginData);
-                getAuthTokens(res.data)
-            } catch(error) {
-                let status = error.response.status;
-                if (status === 401) {
-                    setErrorMessage('Ces identifiants sont incorrects.')
-                } else if (status === 429) {
-                    setErrorMessage("Trop d'essais.")
-                } else if (status === 500) {
-                    setErrorMessage("Erreur serveur.")
-                }
+    const handleLogin = async (data) => {
+        console.log(data);
+        try {
+            let res = await axios.post('/api/token/', data);
+            getAuthTokens(res.data)
+        } catch(error) {
+            let status = error.response.status;
+            if (status === 401) {
+                setErrorMessage('Ces identifiants sont incorrects.')
+            } else if (status === 429) {
+                setErrorMessage("Trop d'essais.")
+            } else if (status === 500) {
+                setErrorMessage("Erreur serveur.")
             }
         }
     };
@@ -47,14 +38,29 @@ const Login = () => {
     return (
         <div className='login'>
             <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                <input onChange={(e) => setUsername(e.target.value)} type='text' placeholder="Nom d'utilisateur" value={username}/>
-                {blankUsername && <p>Veuillez entrer un nom d'utilisateur.</p>}
-                <input onChange={(e) => setPassword(e.target.value)}type='password' placeholder="Mot de passe" value={password}/>
-                {blankPassword && <p>Veuillez entrer un mot de passe.</p>}
-                {errorMessage && <p>{errorMessage}</p>}
-                <input type='submit' value="Se connecter"/>
+            <form onSubmit={handleSubmit(handleLogin)}>
+                <input
+                    type='text'
+                    name='username'
+                    placeholder="Nom d'utilisateur"
+                    {...register('username')}
+                />
+                <span>{errors.username?.message}</span>
+                <input
+                    type='password'
+                    name='password'
+                    placeholder="Mot de passe"
+                    {...register('password')}
+                />
+                <span>{errors.password?.message}</span>
+                <input
+                    type='submit'
+                    value="Se connecter"
+                />
             </form>
+
+            {errorMessage && <span>{errorMessage}</span>}
+
             <Link className={'nav-link'} to ="/register">
                 Register
             </Link>
