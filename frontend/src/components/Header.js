99b118b6d014b723fc66610086e90a00735a4d10
Louack <loic.briset@hotmail.fr>
2021-12-30 09:03:33 +0100

    modified headers style  depending on connexion status

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,15 +5,14 @@ import AuthContext from "../context/AuthContext";
 const Header = () => {
     const { username } = useContext(AuthContext)
 
-    if (username) {
-        return (
-            <header>
-                <Navigation/>
+    return (
+            <header style={username ? 
+                {justifyContent: "space-between", borderBottom: "solid"} : 
+                {justifyContent: "center"}}>
+                <h1>Chess Manager</h1>
+                {username && <Navigation/>}
             </header>
         )
-    } else {
-        return null
-    }
 }
 
 export default Header
