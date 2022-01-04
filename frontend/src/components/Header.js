a468497496c2e300ba2cd2d1aae4822590978159
Louack <loic.briset@hotmail.fr>
2022-01-04 21:38:07 +0100

    added responsive header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,57 @@
-import React, {useContext} from 'react'
+import React, {useState, useEffect} from 'react'
 import Navigation from '../components/Navigation';
-import AuthContext from "../context/AuthContext";
 
 const Header = () => {
-    const { username } = useContext(AuthContext)
+    const [navClass, setNavClass] = useState(() => {
+        if (window.innerWidth > 1400) {
+            return "large-nav"
+        }
+        if (window.innerWidth > 580) {
+            return "medium-nav"
+        }
+        return "compact-nav"
+    })
+
+    const [titleClass, setTitleClass] = useState(() => {
+        if (window.innerWidth > 940) {
+            return "large-title"
+        }
+        return "compact-title"
+    })
+
+    useEffect(() => {
+        const checkWindowWidth = () => {
+            if (window.innerWidth > 1400) {
+                setNavClass("large-nav")
+                setTitleClass("large-title")
+            } else if (window.innerWidth > 940){
+                setNavClass("medium-nav")
+                setTitleClass("large-title")
+            }
+            else if (window.innerWidth > 580){
+                setNavClass("medium-nav")
+                setTitleClass("compact-title")
+            } else {
+                setNavClass("compact-nav")
+                setTitleClass("compact-title")
+            }
+        }
+
+        window.addEventListener("resize", checkWindowWidth)
+
+        return () => {
+            window.removeEventListener("resize", checkWindowWidth)
+        }
+          
+    }, [])
 
     return (
             <header>
-                <div className='header-logo-title'>
+                <div className={titleClass}>
                     <img src="/img/header-chess.jpg" alt="header-chess" />
                     <h1>Chess Manager</h1>
                 </div>
-                {username && <Navigation/>}
+                <Navigation navClass={navClass}/>
             </header>
         )
 }
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -4,19 +4,21 @@ import '../App.css'
 import AuthContext from "../context/AuthContext";
 
 
-const Navigation = () => {
-    const { username, removeAuthTokens } = useContext(AuthContext)
+const Navigation = ( {navClass} ) => {
+    const { removeAuthTokens } = useContext(AuthContext)
     const [openUserMenu, setOpenUserMenu] = useState(false)
     const navRef = useRef()
     const navigate = useNavigate();
 
     const displayUserMenu = () => {
         setOpenUserMenu(!openUserMenu)
+        console.log(openUserMenu)
     }
 
     useEffect(() => {
         const checkOutsideClick = (e) => {
             if (openUserMenu && navRef.current && !navRef.current.contains(e.target)) {
+                console.log('hey')
                 setOpenUserMenu(false)
             }
           }
@@ -30,40 +32,39 @@ const Navigation = () => {
 
     return (
         <>
-            <nav ref={navRef}>
-                <div className="nav-item" onClick={() => navigate("/tournaments")}>
-                    <img src="/img/cup-icon.png" alt="cup-icon" />
-                    <span>
-                        <b>Mes tournois</b>
+            <nav className={navClass} ref={navRef}>
+                <div className="menu-switch" onClick={() => displayUserMenu()}>
+                    <span className="nav-text">
+                        <b>Menu</b>
                     </span>
+                    <img src="/img/arrow-icon.png" alt="arrow-icon" style={openUserMenu ? {transform: 'rotate(-90deg)'} : null}/>
                 </div>
-                <div className="nav-item" onClick={() => navigate("/players")}>
-                    <img src="/img/people-icon.png" alt="people-icon" />
-                    <span>
-                        <b>Mes joueurs</b>
-                    </span>
-                </div>
-                <div className="nav-item" onClick={displayUserMenu}>
-                    <img className="user-menu-img" src="/img/arrow-icon.png" alt="arrow-icon" style={openUserMenu ? {transform: 'rotate(90deg)'} : null}/>
-                    <span>
-                        <b>{username}</b>
-                    </span>
-                </div>
-                {openUserMenu && 
-                <div className='nav-user-menu'>
-                    <div className='user-menu-item' onClick={() => navigate("/profile")}>
+                <div className="nav-menu" style={openUserMenu ? {display: 'flex'} : null}>
+                    <div className="menu-item" onClick={() => navigate("/tournaments")}>
+                        <img src="/img/cup-icon.png" alt="cup-icon" />
+                        <span className="nav-text">
+                            <b>Mes tournois</b>
+                        </span>
+                    </div>
+                    <div className="menu-item" onClick={() => navigate("/players")}>
+                        <img src="/img/people-icon.png" alt="people-icon" />
+                        <span className="nav-text">
+                            <b>Mes joueurs</b>
+                        </span>
+                    </div>
+                    <div className="menu-item" onClick={() => navigate("/profile")}>
                         <img src="/img/profile-icon.png" alt="profile-icon" />
-                        <span>
-                            Mon profil
+                        <span className="nav-text">
+                            <b>Mon profil</b>
                         </span>
                     </div>
-                    <div className='user-menu-item'  onClick={removeAuthTokens}>
+                    <div className="menu-item"  onClick={removeAuthTokens}>
                         <img src="/img/unplug-icon.png" alt="unplug-icon" />
-                        <span>
-                            Déconnexion
+                        <span className="nav-text">
+                            <b>Déconnexion</b>
                         </span>
                     </div>
-                </div>}
+                </div>
             </nav>
         </>
     );
